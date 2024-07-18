e0242d1e900efbc64838a99c7b4f11fef734a9a7
William Daughtridge <wdaughtridge@alarm.com>
2024-07-18 17:10:00 -0400

    new fish stuff

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,18 +9,22 @@ source ~/.devpy/bin/activate.fish
 # PATH
 set -gx PATH ~/.config/emacs/bin ~/.dotnet/tools /opt/homebrew/lib/ruby/gems/3.2.0/bin /opt/homebrew/opt/ruby/bin /opt/homebrew/bin /opt/homebrew/sbin $GOBIN $PATH
 
+fzf --fish | source
+
+function switch_to_repo
+    set selected (find ~/repos -mindepth 1 -maxdepth 1 -type d | fzf)
+    cd $selected
+    commandline -f repaint
+end
+
 # Abbreviations
 abbr -a vim nvim
 abbr -a k kubectl
 abbr -a ta tmux a
 abbr -a cl clear
 
-fzf --fish | source
-
 # Key bindings
-bind \ct ~/.local/bin/tmux-terminal-sessionizer
-bind \cf ~/.local/bin/tmux-sessionizer
-bind \ck ~/.local/bin/tmux-kubeit
+bind \cf switch_to_repo
 
 function fish_prompt
     set_color blue
