97c7cdfebb070e2b7cf5ba19dfde7706998bac74
William Daughtridge <wdaughtridge@alarm.com>
2024-08-14 06:51:47 -0400

    stuff

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,9 +1,11 @@
 vim.api.nvim_create_user_command('Q', 'q', {})
 vim.api.nvim_create_user_command('W', 'w', {})
 
+vim.cmd 'colorscheme wildcharm'
+vim.cmd 'hi Normal guibg=NONE ctermbg=NONE'
+
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-vim.g.have_nerd_font = false
 
 vim.opt.termguicolors = true
 vim.opt.number = true
@@ -43,8 +45,8 @@ vim.keymap.set('n', '<C-g>', 'gt')
 vim.keymap.set('n', '<C-f>', 'gT')
 vim.keymap.set('i', '<C-g>', '<Esc>gt')
 vim.keymap.set('i', '<C-f>', '<Esc>gT')
-vim.keymap.set('t', '<C-g>', '<C-\\><C-n>gt')
-vim.keymap.set('t', '<C-f>', '<C-\\><C-n>gT')
+-- vim.keymap.set('t', '<C-g>', '<C-\\><C-n>gt')
+-- vim.keymap.set('t', '<C-f>', '<C-\\><C-n>gT')
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', '<leader>X', ':read !<C-r>0<CR>')
@@ -79,9 +81,7 @@ require('lazy').setup({
     "tpope/vim-fugitive",
     config = function()
       vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-
       local Fugitive = vim.api.nvim_create_augroup("Fugitive", {})
-
       local autocmd = vim.api.nvim_create_autocmd
       autocmd("BufWinEnter", {
         group = Fugitive,
@@ -90,40 +90,23 @@ require('lazy').setup({
           if vim.bo.ft ~= "fugitive" then
             return
           end
-
           local bufnr = vim.api.nvim_get_current_buf()
           local opts = { buffer = bufnr, remap = false }
           vim.keymap.set("n", "<leader>p", function()
             vim.cmd.Git('push')
           end, opts)
-
-          -- rebase always
           vim.keymap.set("n", "<leader>P", function()
-            vim.cmd.Git({ 'pull', '--rebase' })
+            vim.cmd.Git('pull')
           end, opts)
-
-          -- NOTE: It allows me to easily set the branch i am pushing and any tracking
-          -- needed if i did not set the branch up correctly
-          vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
         end,
       })
-
       vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
       vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
     end
   },
-  { 'numToStr/Comment.nvim',    opts = {} },
-  -- {
-  --   'nvim-orgmode/orgmode',
-  --   event = 'VeryLazy',
-  --   ft = { 'org' },
-  --   config = function()
-  --     require('orgmode').setup({
-  --       org_agenda_files = '~/repos/org/**/*',
-  --       org_default_notes_file = '~/repos/org/refile.org',
-  --     })
-  --   end,
-  -- },
+  {
+    'numToStr/Comment.nvim', opts = {}
+  },
   {
     'stevearc/oil.nvim',
     opts = {},
@@ -155,10 +138,11 @@ require('lazy').setup({
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
       local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>gs', builtin.git_status, { desc = '[G]it [S]tatus' })
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sf', function()
+        builtin.find_files { previewer = false }
+      end, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = '[G]it [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
@@ -180,7 +164,7 @@ require('lazy').setup({
         }
       end, { desc = '[S]earch [/] in Open Files' })
       vim.keymap.set('n', '<leader>sn', function()
-        builtin.find_files { cwd = vim.fn.stdpath 'config' }
+        builtin.find_files { cwd = vim.fn.stdpath 'config', previewer = false, }
       end, { desc = '[S]earch [N]eovim files' })
     end,
   },
@@ -227,7 +211,6 @@ require('lazy').setup({
         },
         terraformls = {},
         bashls = {},
-        -- csharp_ls = {},
         omnisharp = {},
         lua_ls = {
           settings = {
@@ -311,54 +294,14 @@ require('lazy').setup({
       }
     end,
   },
-  -- {
-  --   'navarasu/onedark.nvim',
-  --   priority = 1000,
-  --   init = function()
-  --     vim.cmd.hi 'Comment gui=none'
-  --     vim.cmd.colorscheme 'onedark'
-  --   end,
-  --   config = function()
-  --     require('onedark').setup {
-  --       style = 'darker'
-  --     }
-  --   end
-  -- },
-  -- {
-  --   'ellisonleao/gruvbox.nvim',
-  --   priority = 1000,
-  --   init = function()
-  --     vim.cmd.hi 'Comment gui=none'
-  --     -- vim.cmd.colorscheme 'gruvbox'
-  --   end,
-  -- },
-  {
-    'folke/tokyonight.nvim',
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = 'transparent',
-        floats = 'transparent',
-      },
-    },
-    priority = 1000,
-    init = function()
-      vim.cmd.hi 'Comment gui=none'
-      vim.cmd.colorscheme 'tokyonight-night'
-    end,
-  },
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
       ensure_installed = { 'helm', 'yaml', 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'go', 'c_sharp', 'dockerfile', 'xml' },
       auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
+      highlight = { enable = true },
+      indent = { enable = true },
     },
     config = function(_, opts)
       ---@diagnostic disable-next-line: missing-fields
