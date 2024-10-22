0d945f918718a236ff029998d3499f24da25594d
William Daughtridge <wdaughtridge@alarm.com>
2024-10-22 17:01:48 -0400

    update

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -1,20 +1,31 @@
--- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
-
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-  theme = "ayu_dark",
+  theme = "dark_horizon",
+}
+
+M.ui = {
+  statusline = {
+    separator_style = "round",
+    theme = "minimal"
+  },
+  cmp = {
+    style = "atom"
+  },
+  tabufline = {
+    lazyload = false,
+  },
 }
 
 M.term = {
   float = {
     relative = "editor",
-    row = 0,
-    col = 0,
-    width = 1,
-    height = 1,
-    border = "none",
+    row = 0.05,
+    col = 0.1,
+    width = 0.8,
+    height = 0.8,
+    border = "double",
   },
 }
 
