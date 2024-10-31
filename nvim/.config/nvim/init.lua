3033ea9517573c6f4a4f1ebc23285de32d6c9ba3
William Daughtridge <wdaughtridge@gmail.com>
2024-10-31 06:17:56 -0400

    fix

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -10,5 +10,18 @@ local lspconfig = require('lspconfig')
 lspconfig.gopls.setup{}
 lspconfig.rust_analyzer.setup{}
 lspconfig.lua_ls.setup{}
+lspconfig.elixirls.setup{
+  cmd = { '/opt/homebrew/bin/elixir-ls' };
+}
 
-require("mason").setup{}
+require('mason').setup{}
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline', 'go', 'rust', 'elixir' },
+  sync_install = false,
+  ignore_install = { 'javascript' },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+}
