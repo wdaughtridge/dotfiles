65a085e14e9ba397f9647afed84016e03ee4675a
William Daughtridge <wdaughtridge@gmail.com>
2024-12-05 16:40:39 -0500

    wez

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.font = wezterm.font 'JetBrains Mono'
+config.font_size = 16
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+return config
--- wezterm/.wezterm.lua
+++
@@ -1,7 +0,0 @@
-local wezterm = require 'wezterm'
-local config = {}
-
-config.font = wezterm.font 'JetBrains Mono'
-config.font_size = 16
-
-return config
