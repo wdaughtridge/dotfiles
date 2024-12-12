0e6c18e7512f09bd319650bafde8ff63642ffe4a
William Daughtridge <wdaughtridge@gmail.com>
2024-12-12 16:39:23 -0500

    theme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,11 +1,10 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-config.color_scheme = 'rose-pine'
+config.color_scheme = 'Atom'
 config.font = wezterm.font 'JetBrains Mono'
 config.font_size = 16
 config.use_fancy_tab_bar = false
-config.tab_bar_at_bottom = true
 config.window_padding = {
   left = 0,
   right = 0,
