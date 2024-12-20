e17ca3895f82a0f746c9271b59bb9d071941659e
William Daughtridge <wdaughtridge@gmail.com>
2024-12-20 08:36:07 -0500

    fish

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -26,3 +26,32 @@ require('lazy').setup({
 })
 
 require('telescope').setup{ defaults = { file_ignore_patterns = {"deps"} } }
+
+-- Lua LSP
+local lspconfig = require('lspconfig')
+lspconfig.lua_ls.setup {
+	on_init = function(client)
+		if client.workspace_folders then
+			local path = client.workspace_folders[1].name
+			---@diagnostic disable-next-line: undefined-field
+			if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
+				return
+			end
+		end
+
+		client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+			runtime = {
+				version = 'LuaJIT'
+			},
+			workspace = {
+				checkThirdParty = false,
+				library = {
+					vim.env.VIMRUNTIME
+				}
+			}
+		})
+	end,
+	settings = {
+		Lua = {}
+	}
+}
--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -1,42 +1,15 @@
 local lspconfig = require('lspconfig')
-
+-- Go
 lspconfig.gopls.setup {}
-
+-- Rust
 lspconfig.rust_analyzer.setup {}
-
+-- Deno
 lspconfig.denols.setup {}
-
+-- C#
 lspconfig.omnisharp.setup {
 	cmd = { 'omnisharp' },
 }
-
+-- Elixir
 lspconfig.elixirls.setup {
 	cmd = { 'elixir-ls' },
-}
-
-lspconfig.lua_ls.setup {
-	on_init = function(client)
-		if client.workspace_folders then
-			local path = client.workspace_folders[1].name
-			---@diagnostic disable-next-line: undefined-field
-			if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
-				return
-			end
-		end
-
-		client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-			runtime = {
-				version = 'LuaJIT'
-			},
-			workspace = {
-				checkThirdParty = false,
-				library = {
-					vim.env.VIMRUNTIME
-				}
-			}
-		})
-	end,
-	settings = {
-		Lua = {}
-	}
 }
