1f73782de4750d938489697019d753a51b4d2c52
William Daughtridge <wdaughtridge@gmail.com>
2024-12-20 08:23:49 -0500

    nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,31 +1,47 @@
 require('config.lazy')
 require('config.lsp')
 
+-- -- -- -- --
+-- Keymaps  --
+-- -- -- -- --
+-- Netrw size
 vim.g.netrw_winsize = 25
-
-vim.cmd.colorscheme('wildcharm')
-vim.cmd.hi('Normal guibg=NONE ctermbg=NONE')
+-- Theme
+vim.cmd.colorscheme('kanagawa')
+-- Sign column
 vim.opt.signcolumn = 'yes'
-
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+-- Find files
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>f', builtin.git_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>a', builtin.find_files, { desc = 'Telescope find files' })
+-- Search
+vim.keymap.set('n', '<leader>/', builtin.live_grep, { desc = 'Telescope live grep' })
+-- Show buffers
+vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Telescope buffers' })
+-- File explorer
+vim.keymap.set('n', '<leader>e', vim.cmd.Explore)
+-- Delete buffer
+vim.keymap.set('n', '<leader>q', vim.cmd.bdelete)
+-- LSP
+vim.keymap.set('n', '<leader>k', vim.lsp.buf.hover)
+vim.keymap.set('n', '<leader>l', vim.lsp.buf.format)
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>fa', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>ff', builtin.git_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
-vim.keymap.set('n', '<leader>e', vim.cmd.Explore)
-vim.keymap.set('n', '<leader>x', vim.cmd.bdelete)
+-- Clean highlight
 vim.keymap.set('n', '<Esc>', vim.cmd.nohlsearch)
-vim.keymap.set('n', '<leader>bf', vim.lsp.buf.format)
+-- Go to end, beginning of line
+vim.keymap.set('n', 'gl', '$')
+vim.keymap.set('n', 'gh', '0')
 
+-- Mason
 require('mason').setup {}
 
+-- Treesitter
 require 'nvim-treesitter.configs'.setup {
-	ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline', 'go', 'rust', 'elixir', 'c_sharp' },
+	ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline', 'go', 'rust', 'c_sharp' },
 	sync_install = false,
 	ignore_install = { 'javascript' },
 	highlight = {
