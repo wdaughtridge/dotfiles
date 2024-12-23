a497a9b00b82169b0cd65a2ca73f7b8bc22a0624
William Daughtridge <wdaughtridge@gmail.com>
2024-12-23 08:15:50 -0500

    remove fg

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -24,16 +24,11 @@ if test -d ~/.kube
     end
     bind \ck switch_kubeconfig
 end
-# Fg + bg toggle
-function toggle_fg_proc
-    fg 2&>/dev/null
-end
 # Abbreviations
 abbr -a k kubectl
 abbr -a erase_kubeconfig set -e KUBECONFIG
 # Key bindings
 bind \cf switch_to_repo
-bind \cz toggle_fg_proc
 bind \ct ~/.local/bin/tmux-terminal-sessionizer
 # Bat 🦇
 set -x BAT_THEME "Visual Studio Dark+"
