013d2c2eb1490567874332f4ce87837107e9b240
William Daughtridge <wdaughtridge@gmail.com>
2025-01-21 10:40:40 -0500

    emacs

---
+++ emacs/.emacs.d/lisp/packages.el
@@ -0,0 +1,21 @@
+(use-package doom-themes
+  :ensure t
+  :config
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t)
+  (load-theme 'doom-sourcerer t)
+  (doom-themes-visual-bell-config)
+  (doom-themes-org-config))
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (when (file-directory-p "~/Developer")
+    (setq projectile-project-search-path '("~/Developer")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package magit
+  :commands (magit-status magit-get-current-branch))
