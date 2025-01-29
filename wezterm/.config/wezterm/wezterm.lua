615391ef5c46cbf93f8d0b53eaddb8fc6f822fd3
William Daughtridge <wdaughtridge@gmail.com>
2025-01-29 14:59:34 -0500

    nvim

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,16 +1,16 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
-config.color_scheme = 'Arthur'
-config.font = wezterm.font 'JetBrains Mono SemiBold'
-config.font_size = 18
+config.color_scheme = "Arthur"
+config.font = wezterm.font("JetBrains Mono SemiBold")
+config.font_size = 14
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 return config
