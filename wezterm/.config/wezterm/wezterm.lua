4d0bee7fab9f6bedc8f8051d2e64329a7c90e090
William Daughtridge <wdaughtridge@gmail.com>
2025-02-03 11:24:34 -0500

    nvim and tmux
    back to basics

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,17 +1,10 @@
 local wezterm = require("wezterm")
 local config = {}
 
-config.color_scheme = "Ayu Dark (Gogh)"
-
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
 config.font = wezterm.font("0xProto Nerd Font")
 config.font_size = 18
-config.use_fancy_tab_bar = false
-config.tab_bar_at_bottom = true
-config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
-}
 
 return config
