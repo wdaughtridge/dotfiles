d2e46f1c5585d9b4a4825185e29a9e82aa5d2e92
Will Daughtridge <wdaughtridge@alarm.com>
2025-03-06 10:35:12 -0500

    fix

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "gopls", "rust_analyzer", "elp" }
+local servers = { "html", "cssls", "gopls", "rust_analyzer", "elp", "terraformls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
