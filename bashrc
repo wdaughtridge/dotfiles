68cfc228b7fe35c6bfb6faa51eedcb3666ff1e41
Will Daughtridge <wdaughtridge@alarm.com>
2025-03-10 16:20:05 -0400

    fix

--- bashrc
+++ bashrc
@@ -2,5 +2,5 @@
 alias ll='ls -la'
 export GOPATH="$HOME/go"
 export GOBIN="$GOPATH/bin"
-export PATH="$GOBIN:$HOME/.local/bin:/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+export PATH="$GOBIN:$HOME/.local/bin:/opt/homebrew/bin:/opt/homebrew/sbin:$HOME/.config/emacs/bin:$PATH"
 PS1="\w\$ "
---
+++ custom.el
@@ -0,0 +1,14 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("2b20b4633721cc23869499012a69894293d49e147feeb833663fdc968f240873" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
