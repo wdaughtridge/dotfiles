5cf318634c96e1996feba2fa673bd8eae98aaa76
Will Daughtridge <wdaughtridge@alarm.com>
2025-03-23 10:44:40 -0400

    fix

--- config.fish
+++ config.fish
@@ -1,4 +1,7 @@
 set fish_greeting
-fish_config theme choose ayu\ Light
+fish_config theme choose Base16\ Default\ Dark
 source $HOME/.cargo/env.fish
-set -x PATH $HOME/.local/bin /opt/homebrew/bin /opt/homebrew/sbin $PATH
+alias http3-client $HOME/Developer/quiche/target/release/examples/http3-client
+set -x GOPATH $HOME/go
+set -x GOBIN $GOPATH/bin
+set -x PATH $GOBIN $HOME/.config/emacs/bin $HOME/.local/bin /opt/homebrew/bin /opt/homebrew/sbin $PATH
