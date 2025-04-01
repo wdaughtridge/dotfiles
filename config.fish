424f25ba32fbbd29f4121c904165d8f6d42575ed
Will Daughtridge <wdaughtridge@gmail.com>
2025-04-01 15:45:30 -0400

    feat

--- config.fish
+++ config.fish
@@ -1,7 +1,8 @@
 set fish_greeting
-fish_config theme choose Base16\ Default\ Dark
+fish_config theme choose ayu\ Light
 source $HOME/.cargo/env.fish
 alias http3-client $HOME/Developer/quiche/target/release/examples/http3-client
+set -x DOTNET_ROOT /opt/homebrew/opt/dotnet/libexec
 set -x GOPATH $HOME/go
 set -x GOBIN $GOPATH/bin
-set -x PATH $GOBIN $HOME/.config/emacs/bin $HOME/.local/bin /opt/homebrew/bin /opt/homebrew/sbin $PATH
+set -x PATH $GOBIN $HOME/.dotnet/tools $HOME/.config/emacs/bin $HOME/.local/bin /opt/homebrew/bin /opt/homebrew/sbin $PATH
