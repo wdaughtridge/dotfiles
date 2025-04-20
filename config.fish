80f445e0ec4f8ae283db390da6b5d08fdbe893de
Will Daughtridge <wdaughtridge@gmail.com>
2025-04-20 16:35:08 -0400

    feat - let there be light!

--- config.fish
+++ config.fish
@@ -1,7 +1,16 @@
 set fish_greeting
-fish_config theme choose ayu\ Dark
+fish_config theme choose ayu\ Mirage
 source $HOME/.cargo/env.fish
-alias http3-client $HOME/Developer/quiche/target/release/examples/http3-client
+function lightmode
+	touch $HOME/.lightmode
+	source $HOME/.config/fish/config.fish
+end
+function darkmode
+	rm $HOME/.lightmode
+	source $HOME/.config/fish/config.fish
+end
+set -x VISUAL nvim
+set -x EDITOR nvim
 set -x DOTNET_ROOT /usr/local/share/dotnet
 set -x GOPATH $HOME/go
 set -x GOBIN $GOPATH/bin
