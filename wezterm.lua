1db3f92d111bf48e40a1ec901844ae308afc2f71
Will Daughtridge <wdaughtridge@gmail.com>
2025-05-21 16:10:43 -0400

    feat

--- wezterm.lua
+++ wezterm.lua
@@ -1,18 +1,18 @@
-local wezterm = require('wezterm')
-local io = require('io')
-local os = require('os')
+local wezterm = require("wezterm")
+local io = require("io")
+local os = require("os")
 
 local config = wezterm.config_builder()
 local action = wezterm.action
-local projects_dir = wezterm.home_dir .. '/Developer'
+local projects_dir = wezterm.home_dir .. "/Developer"
 
 local home = os.getenv("HOME")
 local light = io.open(home .. "/.lightmode","r")
 if light ~= nil then
   io.close(light)
-  config.color_scheme = 'Windows 10 Light (base16)'
+  config.color_scheme = "Windows 10 Light (base16)"
 else
-  config.color_scheme = 'Windows 10 (base16)'
+  config.color_scheme = "Windows 10 (base16)"
 end
 
 -- Opacity
@@ -24,46 +24,46 @@ config.use_fancy_tab_bar = false
 
 -- Old tmux leader I am used to
 config.leader = {
-  key = '\\',
-  mods = 'CTRL',
+  key = "\\",
+  mods = "CTRL",
   timeout_milliseconds = 1000
 }
 
 -- Update the workspace name in window
-wezterm.on('update-right-status', function(window, _)
+wezterm.on("update-right-status", function(window, _)
   window:set_right_status(window:active_workspace())
 end)
 
 -- Poll for workspace change in temp file
-wezterm.on('poll-workspace', function(window, pane)
+wezterm.on("poll-workspace", function(window, pane)
   -- This is set in the caller when workspace change is requested. I do wish
   -- we could pass this as an argument though
   local temp_file = wezterm.GLOBAL.workspace_temp_file
 
   -- Try to open the temp file which will probably always work
-  local f = io.open(temp_file, 'r')
+  local f = io.open(temp_file, "r")
   if f ~= nil then
-    local result = f:read('*l')
+    local result = f:read("*l")
     f:close()
 
     -- Is there anything in the file?
     if result ~= nil and #result > 0 then
       os.remove(temp_file)
 
-      wezterm.log_info('Selected workspace: ' .. result)
-      wezterm.log_info('Working dir: ' .. projects_dir .. '/' .. result)
+      wezterm.log_info("Selected workspace: " .. result)
+      wezterm.log_info("Working dir: " .. projects_dir .. "/" .. result)
 
       -- Actually switch to the new workspace with some environment
       -- variables set. My fish config checks for CWD, and if it is
-      -- present, cd's to it
+      -- present, cd"s to it
       window:perform_action(
         action.SwitchToWorkspace {
           name = result,
 	  spawn = {
             set_environment_variables = {
-              CWD = projects_dir .. '/' .. result,
+              CWD = projects_dir .. "/" .. result,
             },
-            args = { '/opt/homebrew/bin/fish' },
+            args = { "/opt/homebrew/bin/fish" },
           },
         },
         pane
@@ -71,33 +71,48 @@ wezterm.on('poll-workspace', function(window, pane)
     else
       -- Recurse!
       window:perform_action(
-        action.EmitEvent('poll-workspace'),
+        action.EmitEvent("poll-workspace"),
         pane
       )
     end
   end
 end)
 
--- Gitui (could be lazygit, too)
-wezterm.on('gitui', function(window, _)
-  window:mux_window():spawn_tab {
+-- Git UI
+wezterm.on("lazygit", function(window, _)
+  local tabs = window:mux_window():tabs()
+
+  -- Activate lazygit tab if exists
+  for _, tab in pairs(tabs) do
+    local title = tab:get_title()
+    if title == "lazygit" then
+      tab:activate()
+      return
+    end
+  end
+
+  -- Otherwise we need to create a new tab
+  local tab, _, _ = window:mux_window():spawn_tab {
     args = {
-      '/opt/homebrew/bin/gitui'
+      "/opt/homebrew/bin/lazygit"
     },
   }
+
+  -- And set the title so we can query it later
+  tab:set_title("lazygit")
 end)
 
-wezterm.on('switch-workspace', function(window, pane)
+wezterm.on("switch-workspace", function(window, pane)
   local temp_file = os.tmpname()
-  local projects = ''
+  local projects = ""
 
   -- Just list out files in the projects dir... I wonder if there
   -- is a better way to do this without invoking ls
-  for dir in io.popen('ls ' .. projects_dir):lines() do
-    if projects == '' then
+  for dir in io.popen("ls " .. projects_dir):lines() do
+    if projects == "" then
       projects = dir
     else
-      projects = projects .. '\n' .. dir
+      projects = projects .. "\n" .. dir
     end
   end
 
@@ -105,75 +120,75 @@ wezterm.on('switch-workspace', function(window, pane)
   -- maybe not?
   window:mux_window():spawn_tab {
     args = {
-      '/opt/homebrew/bin/fish', '-c',
-      string.format('echo "%s" | /opt/homebrew/bin/fzf > %s', projects, temp_file)
+      "/opt/homebrew/bin/fish", "-c",
+      string.format("echo '%s' | /opt/homebrew/bin/fzf > %s", projects, temp_file)
     },
   }
 
   -- TODO: can we pass this as an argument?
   wezterm.GLOBAL.workspace_temp_file = temp_file
-  wezterm.log_info('Temp file: ' .. wezterm.GLOBAL.workspace_temp_file)
+  wezterm.log_info("Temp file: " .. wezterm.GLOBAL.workspace_temp_file)
 
   -- Actually poll for a response from user
   window:perform_action(
-    action.EmitEvent('poll-workspace'),
+    action.EmitEvent("poll-workspace"),
     pane
   )
 end)
 
-wezterm.on('split-horizontal', function(_, pane)
+-- Tmux-like splits
+wezterm.on("split-horizontal", function(_, pane)
   pane:split {}
 end)
-
-wezterm.on('split-vertical', function(_, pane)
-  pane:split { direction = 'Bottom' }
+wezterm.on("split-vertical", function(_, pane)
+  pane:split { direction = "Bottom" }
 end)
 
 config.keys = {
+  -- Split down
   {
-    key = '"',
-    mods = 'LEADER',
-    action = action.EmitEvent('split-vertical'),
+    key = "\"",
+    mods = "LEADER",
+    action = action.EmitEvent("split-vertical"),
   },
+  -- Split right
   {
-    key = '%',
-    mods = 'LEADER',
-    action = action.EmitEvent('split-horizontal'),
+    key = "%",
+    mods = "LEADER",
+    action = action.EmitEvent("split-horizontal"),
   },
+  -- Search workspaces
   {
-    key = 'w',
-    mods = 'LEADER',
+    key = "s",
+    mods = "LEADER",
     action = action.ShowLauncherArgs {
-      flags = 'FUZZY|WORKSPACES',
+      flags = "FUZZY|WORKSPACES",
     },
   },
+  -- Go to 'terminal' session
   {
-    key = 's',
-    mods = 'LEADER',
-    action = action.ShowLauncherArgs {
-      flags = 'WORKSPACES',
-    },
-  },
-  {
-    key = 't',
-    mods = 'LEADER',
+    key = "t",
+    mods = "LEADER",
     action = action.SwitchToWorkspace {
-      name = 'default',
+      name = "default",
     },
   },
+  -- Lazygit
   {
-    key = 'g',
-    mods = 'LEADER',
-    action = action.EmitEvent('gitui'),
+    key = "g",
+    mods = "LEADER",
+    action = action.EmitEvent("lazygit"),
   },
+  -- Find workspace from projects directory
   {
-    key = 'f',
-    mods = 'LEADER',
-    action = action.EmitEvent('switch-workspace'),
+    key = "f",
+    mods = "LEADER",
+    action = action.EmitEvent("switch-workspace"),
   },
+  -- WezTerm debug
   {
-    key = 'L',
-    mods = 'CTRL',
+    key = "L",
+    mods = "CTRL",
     action = action.ShowDebugOverlay
   },
 }
