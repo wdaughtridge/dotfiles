13debf3a11d11f1cdf65c43986b5a759fe6f1aad
Will Daughtridge <wdaughtridge@gmail.com>
2025-05-23 10:10:49 -0400

    feat

--- wezterm.lua
+++ wezterm.lua
@@ -15,6 +15,15 @@ else
   config.color_scheme = "Windows 10 (base16)"
 end
 
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+config.window_decorations = "RESIZE"
+
 -- Opacity
 -- This is not so nice when stage manager is off
 config.window_background_opacity = 0.9
